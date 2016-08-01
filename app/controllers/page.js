89bc889eee05ea0aeb0b445944877836b93452c2
Summerlve <dzxx0563zh@126.com>
2016-08-01 23:37:33 +0800

    prepare jobs done

--- app/controllers/page.js
+++ app/controllers/page.js
@@ -1,7 +1,12 @@
 "use strict";
 
+const { viewRoot } = require("../../index.js");
+const path = require("path");
+
 module.exports.index = (req, res) => {
-    res.send("1");
+    res.render("page.html", function(err, html) {
+        res.send(html);
+    });
 };
 
 module.exports.one = (req, res) => {
--- index.js
+++ index.js
@@ -20,11 +20,17 @@ app.use("/static",
     express.static(
         path.join(appRoot, "public")));
 
+// use ejs engine
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
+// set view root dir
+app.set("views", path.join(appRoot, "app", "views"));
+
 // load route
 const router = require("./route.js");
 app.use("/", router);
 
 // listen port, default port is 9000 just for test
 const { app: { production: port = 9000} } = config;
-
 app.listen(port);
