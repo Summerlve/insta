3fbca7a6c16cf2cf76990e51a8bdf7441259eaef
Summerlve <dzxx0563zh@126.com>
2016-08-10 19:06:01 +0800

    setting gui finish

--- app/assets/root/src/components/Setting.vue
+++ app/assets/root/src/components/Setting.vue
@@ -30,8 +30,15 @@
             <div class="ui grid container">
                 <div class="sixteen wide computer sixteen wide tablet sixteen wide mobile column">
                     <div class="field">
-                        <label>Avatar</label>
-                        <input type="text" name="first-name" placeholder="First Name">
+                        <div class="fluid big ui top attached button" tabindex="0" id="choose-image">
+                            Choose A Picture
+                        </div>
+                        <input type="file" name="img" id="image-input-hidden">
+                        <div class="ui attached center aligned segment">
+                            <i class="file image outline icon" id="image-placeholder">
+                            </i>
+                            <img class="ui medium circular centered image" id="image-preview">
+                        </div>
                     </div>
                 </div>
                 <div class="sixteen wide computer sixteen wide tablet sixteen wide mobile column">
@@ -72,12 +79,52 @@
 
 <script>
     export default {
+        ready() {
+            this.imgSelInit();
+        },
+        methods: {
+            imgSelInit() {
+                // delegate hidden file input click event
+                $("#choose-image").on("click", event => {
+                    $("#image-input-hidden").click();
+                    return false;
+                });
 
+                // image preview
+                $("#image-input-hidden").change(event => {
+                    $("#image-placeholder").hide();
+
+                    this.previewImg(
+                        document.querySelector("#image-input-hidden"),
+                        $("#image-preview"));
+                });
+            },
+            previewImg(source, target) {
+                if (source.files && source.files[0])
+                {
+                    const reader = new FileReader();
+
+                    reader.onload = event => {
+                        $(target).attr("src", event.target.result);
+                    };
+
+                    reader.readAsDataURL(source.files[0]);
+                }
+            }
+        }
     };
 </script>
 
 <style scoped>
     #setting {
         width: 100%;
+    }
+
+    #image-input-hidden {
+        display: none;
+    }
+
+    #image-placeholder {
+        font-size: 200px;
     }
 </style>
